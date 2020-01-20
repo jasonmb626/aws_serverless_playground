b935167c9e04fff8137b12a2d6e637d16572fcee
Jason Brunelle <jason@jasonbrunelle.com>
2020-01-20 13:31:23 -0600

    Fix type of hello function

--- handler.ts
+++ handler.ts
@@ -1,12 +1,8 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { Handler } from 'aws-lambda';
 import 'source-map-support/register';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+export const hello: Handler = async (_event, _context) => {
   return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!'
   };
 }
